59d56e9d75e5744686f2fb6ac3ae81128f0c7d7a
fadealway <ittipatpt@gmail.com>
2016-12-08 16:42:12 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  
+  // test = {table:[{a:"1",b:"2"},{a:"9",b:"9"}]}
+
+  // hero = 'Windstorm';
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
+  
 }
+
